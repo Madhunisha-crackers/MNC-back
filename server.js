8e2e8d330507331ca363f12cbee85942d57eaf5e
phoenixcrackers <phoenixcrackersfwc@gmail.com>
2025-07-29 19:54:55 +0530

    ppp

--- server.js
+++ server.js
@@ -9,11 +9,7 @@ const upload = multer({
 });
 const app = express();
 
-app.use(cors({
-  origin: '*', // Allow all origins
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'], // Allow these methods
-  allowedHeaders: ['Content-Type', 'Authorization'], // Allow these headers
-}));
+app.use(cors('*'));
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 
